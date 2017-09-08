3f5d07c2a4b0a09bcceaf5fd51101567617147ed
Faisal <faisal@kumparan.com>
2017-09-09 03:43:07 +0700

    fix - add StatusBarManager

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { NativeModules, Platform } from "react-native";
 
+const { StatusBarManager } = NativeModules;
+
 export const STATUSBAR_HEIGHT =
   Platform.OS === "ios" ? 20 : StatusBarManager.HEIGHT;
